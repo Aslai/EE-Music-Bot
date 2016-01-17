3d330eb54a8e717e47abd431bf12b3880ab1edde
Kaslai <kaslai@kaslai.com>
2016-01-17 00:48:07 -0800

    Fix tempo bug, improve filename parser, fix new init packet

--- EEMidiBot/Program.cs
+++ EEMidiBot/Program.cs
@@ -211,8 +211,38 @@ namespace EEMidiBot
 			return offset;
 		}
 
+		public static byte[] ReadFully(Stream input)
+		{
+			byte[] buffer = new byte[16*1024];
+			using (MemoryStream ms = new MemoryStream())
+			{
+				int read;
+				while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+				{
+					ms.Write(buffer, 0, read);
+				}
+				return ms.ToArray();
+			}
+		}
+
 		public bool Read(string fname){
-			byte[] data = File.ReadAllBytes (fname);
+			byte[] data = null;
+			if (fname [fname.Length - 1] == ' ') {
+				fname = fname.Substring (0, fname.Length - 1);
+			} 
+			fname = fname.Trim('\'', '"');
+			if (fname.Substring (0, 7) == "file://") {
+				fname = System.Net.WebUtility.UrlDecode(fname.Substring (7));
+			}
+			if (fname.Substring (0, 7) == "http://") {
+				System.Net.WebClient client = new System.Net.WebClient();
+				Stream stream = client.OpenRead(fname);
+				System.Threading.Thread.Sleep (2000); //Yes. Bad hack...
+				data = ReadFully (stream);
+			}
+			else{
+				data = File.ReadAllBytes (fname);
+			}
 			//Don't bother with exception passing, because that's silly... Right?
 			if (data.Length < 14) {
 				MyLastError = "Invalid Midi file (Too small)";
@@ -248,10 +278,15 @@ namespace EEMidiBot
 				MyLastError = "Invalid Midi file (More tracks were declared than do exist)";
 				return false;
 			}
+			valids = new bool[tracksFound];
+			for (int i = 0; i < tracksFound; ++i) {
+				valids [i] = true;
+			}
 			return true;
 		}
-		
+
 		private long OldTime;
+		private bool[] valids;
 		public void Rewind(){
 			foreach (Track t in MyTracks) {
 				t.Rewind ();
@@ -265,6 +300,7 @@ namespace EEMidiBot
 			long newTime = 0x7FFFFFFF;
 			Event ret = new Event ();
 			Track whichTrack = new Track ();
+			int trk = 0;
 			foreach (Track t in MyTracks) {
 				Event e = t.GetNext ();
 				if (e.Valid && e.Time < newTime && e.Time >= OldTime ) {
@@ -272,6 +308,13 @@ namespace EEMidiBot
 					whichTrack = t;
 					newTime = e.Time;
 				}
+				if (!e.Valid) {
+					valids [trk] = false;
+				}
+				if (e.Valid && !valids [trk]) {
+					Console.WriteLine ("Wow!");
+				}
+				++trk;
 			}
 			if (ret.Status != 0) {
 				ret = ret.Copy ();
@@ -314,19 +357,60 @@ namespace EEMidiBot
 		//private static int WorldWidth = 0;
 		private static int WorldHeight = 0;
 
-		
+		private class Worker
+		{
+			private PlayerIOClient.Connection con;
+			private Midi m;
+			public Worker(PlayerIOClient.Connection othercon, Midi otherm){
+				con = othercon;
+				m = otherm;
+			}
+			public void DoWork(){
+				MainClass.Write (con, m);
+			}
+		}
+
 		private static void onMessage(object sender, PlayerIOClient.Message m)
 		{
 			//Init is the only event we really care about for this bot.
 			switch (m.Type) {
-				case "init":
+			case "init":
 				{
 					WorldKey = derot (m.GetString (5));
-					//WorldWidth = m.GetInt (17);
-					WorldHeight = m.GetInt (18);
-					return;
+					//WorldWidth = m.GetInt (18);
+					WorldHeight = m.GetInt (19);
+					
 				}
+				return;
+			//Definitely not a smart thing to allow others to do...
+			/*
+			case "say":
+				{
+					if (m.GetString (1).Substring (0, 6) != "!midi ") {
+						break;
+					}
+					try {
+						for (int i = 0; i < 16; ++i) {
+							chans [i] = true;
+						}
+						Midi mid = new Midi ();
+						if (!mid.Read (m.GetString (1).Substring (6))) {
+							Console.WriteLine ("Failed to load file. Reason:");
+							Console.WriteLine (mid.LastError);
+						}
+						mid.Rewind ();
+						Worker args = new Worker(MyConnection, mid);
+						System.Threading.Thread t = new System.Threading.Thread( args.DoWork );
+						t.Start();
+						
+					} catch (Exception e) {
+						Console.WriteLine (e.Message);
+					}
+				}
+				break;
+				*/
 			}
+
 		}
 
 		//A quick and dirty map from MIDI percussion to EE percussion
@@ -338,7 +422,17 @@ namespace EEMidiBot
 			10, 19, 19, 19, 19, 19, 6, 17, 17, 17, 17
 		};
 
-		private static void Write(PlayerIOClient.Connection con, Midi m){
+		private static bool[] chans = new bool[16];
+		private static int stop = 0;
+		public static void Write(PlayerIOClient.Connection con, Midi m){
+			//If calling from multiple threads, ensure the existing one aborts.
+			if (stop == 2) {
+				stop = 1;
+				while (stop == 1) {
+					System.Threading.Thread.Sleep (100);
+				}
+			}
+			stop = 2;
 			con.Send ("clear");
 			System.Threading.Thread.Sleep (100);
 			long x = 1;
@@ -348,7 +442,9 @@ namespace EEMidiBot
 			int id = 1;
 			long xprev = 1;
 			long tempo = 60000000 / 120; //Default microseconds per beat (120 bpm)
-			while (true) {
+			long tempobase = 0; //
+			long tempooffset = 0; //
+			while (stop == 2) {
 				if (Console.KeyAvailable) {
 					if (Console.ReadKey ().KeyChar == 'q') {
 						break;
@@ -358,23 +454,27 @@ namespace EEMidiBot
 				if (!e.Valid) {
 					break;
 				}
+				long timestamp = (e.Time - tempobase) * tempo / m.TicksPerQuarter / (1000000L / 85L) + tempooffset;
 				if (e.Status == 0xFF) {
 					//On meta event, I only really care about tempo here.
 					if (e.Arg1 == 0x51 && e.Arg2 == 3) {
+						tempooffset = timestamp;
+						tempobase = e.Time;
 						tempo = (e.Data [0] << 16 ) + (e.Data[1] << 8 ) + e.Data[2];
+
 					}
 				}
 				if ((e.Status >> 4) == 9) {
 					int note = e.Arg1;
-					for (int i = 3; i < e.Arg2; i += 85) { 	//If the note volume is 3 or less, don't emit a note.
-															//If the note volume is 88 or higher, emit two notes.
+					for (int i = 3; i < e.Arg2; i += 805) { 	//If the note volume is 3 or less, don't emit a note.
+						//If the note volume is 88 or higher, emit two notes.
 						//e.Time is in ticks, tempo is in microseconds per tick.
 						//e.Time * tempo / m.TicksPerQuarter = the event time in microseconds.
 						//Divide that by how long each block should represent (I use 1/85th of a second)
-						if (e.Time * tempo / m.TicksPerQuarter / (1000000L / 85L) <= offset) {
+						if (timestamp <= offset) {
 							offset++;
 						} else {
-							offset = e.Time * tempo / m.TicksPerQuarter / (1000000L / 85L);
+							offset = timestamp;
 						}
 
 						//Calculate the X/Y coordinates based on time. This does NOT account for time spent in portals.
@@ -382,18 +482,20 @@ namespace EEMidiBot
 						x = offset / distance + 2;
 						y = offset % distance + 3;
 
-						//If the channel is 9 (AKA MIDI channel 10)
-						if ((e.Status & 0xF) == 9) {
-							//Deal with drums
-							if (note >= 35 && note <= 81) {
-								con.Send (WorldKey, 0, x, y, 83, percussion[note - 35]);
+						if (chans [(e.Status & 0xF)]) {
+							//If the channel is 9 (AKA MIDI channel 10)
+							if ((e.Status & 0xF) == 9) {
+								//Deal with drums
+								if (note >= 35 && note <= 81) {
+									con.Send (WorldKey, 0, x, y, 83, percussion [note - 35]);
+									System.Threading.Thread.Sleep (30);
+									break;
+								}
+							} else {
+								//C3 is note 48 in MIDI, and note 0 in EE
+								con.Send (WorldKey, 0, x, y, 77, note - 48);
 								System.Threading.Thread.Sleep (30);
-								break;
 							}
-						} else {
-							//C3 is note 48 in MIDI, and note 0 in EE
-							con.Send (WorldKey, 0, x, y, 77, note - 48);
-							System.Threading.Thread.Sleep (30);
 						}
 					}
 				}
@@ -410,49 +512,49 @@ namespace EEMidiBot
 				}
 				xprev = x;
 			}
-
 			//Fill in the last column of portals
 			con.Send (WorldKey, 0, x, 2, 242, 1, id, id - 1);
 			System.Threading.Thread.Sleep (30);
-			con.Send (WorldKey, 0, x, WorldHeight - 3, 242, 1, id + 1, 1);
+			con.Send (WorldKey, 0, x, WorldHeight - 3, 242, 1, id + 1, 3);
+			stop = 0;
 		}
 
 		private static PlayerIOClient.Client DoLogin (int method){
 			PlayerIOClient.Client client = null;
 			switch (method) {
 				case 1: {
-					Console.Write ("Username: ");
-					string name = Console.ReadLine ();
+				Console.Write ("Username: ");
+				string name = Console.ReadLine ();
 
-					Console.Write ("Password: ");
-					string pass = Console.ReadLine ();
+				Console.Write ("Password: ");
+				string pass = Console.ReadLine ();
 
-					client = PlayerIO.QuickConnect.SimpleConnect ("everybody-edits-su9rn58o40itdbnw69plyw", name, pass, new string[0]);
-				} break;
+				client = PlayerIO.QuickConnect.SimpleConnect ("everybody-edits-su9rn58o40itdbnw69plyw", name, pass, new string[0]);
+			} break;
 				case 2: {
-					Console.Write ("OAuth Token: ");
-					string name = Console.ReadLine ();
+				Console.Write ("OAuth Token: ");
+				string name = Console.ReadLine ();
 
-					client = PlayerIO.QuickConnect.FacebookOAuthConnect ("everybody-edits-su9rn58o40itdbnw69plyw", name, "", new string[0]);
-				} break;
+				client = PlayerIO.QuickConnect.FacebookOAuthConnect ("everybody-edits-su9rn58o40itdbnw69plyw", name, "", new string[0]);
+			} break;
 				case 3: {
-					Console.Write ("User ID: ");
-					string name = Console.ReadLine ();
+				Console.Write ("User ID: ");
+				string name = Console.ReadLine ();
 
-					Console.Write ("Auth Token: ");
-					string pass = Console.ReadLine ();
+				Console.Write ("Auth Token: ");
+				string pass = Console.ReadLine ();
 
-					client = PlayerIO.QuickConnect.KongregateConnect ("everybody-edits-su9rn58o40itdbnw69plyw", name, pass, new string[0]);
-				} break;
-			default:
+				client = PlayerIO.QuickConnect.KongregateConnect ("everybody-edits-su9rn58o40itdbnw69plyw", name, pass, new string[0]);
+			} break;
+				default:
 				throw new InvalidDataException ("Unknown login method for input \"" + method.ToString() + "\".");
 			}
 			return client;
 		}
-
+		private static PlayerIOClient.Connection MyConnection = null;
 		public static void Main (string[] args)
 		{
-			
+
 			try {
 				Console.WriteLine ("EE Midi Bot - Copyright (C) 2016 Andrew Story");
 				Console.WriteLine ("This program comes with ABSOLUTELY NO WARRANTY. As this is free software, you");
@@ -462,16 +564,16 @@ namespace EEMidiBot
 
 
 				Console.Write ("\n\nWelcome to the EE midi bot.\n\n" +
-					"What login method would you like to use?\n" +
-					"1) Simple\n" +
-					"2) Facebook\n" +
-					"3) Kongregate\n\n");
+				               "What login method would you like to use?\n" +
+				               "1) Simple\n" +
+				               "2) Facebook\n" +
+				               "3) Kongregate\n\n");
 				Console.Write("Enter your choice: ");
 
 				string response = Console.ReadLine ();
 				int value = 0;
 				value = Convert.ToInt32 (response);
-				
+
 				PlayerIOClient.Client client = DoLogin(value);
 
 				Console.Write ("Room ID to join: ");
@@ -479,6 +581,7 @@ namespace EEMidiBot
 				Console.WriteLine ("\n");
 
 				PlayerIOClient.Connection con = client.Multiplayer.JoinRoom (roomid, new Dictionary<string, string> ());
+				MyConnection = con;
 				HandleConnection(con);
 
 			} catch (PlayerIOError e) {
@@ -521,31 +624,57 @@ namespace EEMidiBot
 				string[] cmds = cmd.Split (' ');
 				switch (cmds [0]) {
 					case "midi":
-						try{
-							m = new Midi ();
-							if (!m.Read (cmd.Substring(5))) {
-								Console.WriteLine ("Failed to load file. Reason:");
-								Console.WriteLine (m.LastError);
-							}
-						} catch( Exception e ){
-							Console.WriteLine (e.Message);
+					try{
+						for( int i = 0; i < 16; ++i ){
+							chans[i] = true;
 						}
+						m = new Midi ();
 						Console.WriteLine ("Successfully loaded.");
+						if (!m.Read (cmd.Substring(5))) {
+							Console.WriteLine ("Failed to load file. Reason:");
+							Console.WriteLine (m.LastError);
+						}
+					} catch( Exception e ){
+						Console.WriteLine (e.Message);
+					}
 					break;
 					case "write":
-						Console.WriteLine ("Writing to world (This will take a while...)\nPress q to abort.");
-						m.Rewind ();
-						Write (con, m);
-						Console.WriteLine ("Done!");
-						break;
+					Console.WriteLine ("Writing to world (This will take a while...)\nPress q to abort.");
+					m.Rewind ();
+					Write (con, m);
+					Console.WriteLine ("Done!");
+					break;
 					case "quit": case "exit": case "close":
-						throw new Exception ("Exiting...");
+					throw new Exception ("Exiting...");
+					case "strip":
+					foreach (string s in cmds) {
+						try {
+							chans [Convert.ToInt16 (s)] = false;
+						} catch (Exception e) {
+
+						}
+					}
+					break;
+					case "unstrip":
+					foreach (string s in cmds) {
+						try {
+							chans [Convert.ToInt16 (s)] = true;
+						} catch (Exception e) {
+
+						}
+					}
+					break;
 					case "help":
-						Console.WriteLine("Available commands are:\n midi [path to midi file] - Load a midi file\n write - Save the midi file to the level\n help - Display this message.\n");
-						break;
+					Console.WriteLine("Available commands are:\n" +
+						" midi [path to midi file] - Load a midi file\n" +
+						" write - Save the midi file to the level\n" +
+						" strip [0-15] - Remove a channel from the midi file (9 is drums)\n" +
+						" unstrip [0-15] - Add a channel back to the midi\n" +
+						" help - Display this message.\n");
+					break;
 					default:
-						Console.WriteLine("Unknown command. Try `help`.");
-						break;
+					Console.WriteLine("Unknown command. Try `help`.");
+					break;
 				}
 			}
 		}
